8efa17ab1ca43ba1ef036980101b9c4b4e3f008f
izakfr <izakfr@umich.edu>
2018-04-15 19:51:15 -0400

    Add locks to server

--- restAPI.py
+++ restAPI.py
@@ -1,17 +1,15 @@
 # This file will contain the server that runs and takes GET and POST requests
 # Created by Izak Fritz 04-09-18
 # For the Stably team
+import time
+import threading
 
 from flask import Flask
 from flask import jsonify
 from flask import request
+from queue import Queue
 
 import bittrexWrapper
-
-app = Flask(__name__)
-
-# Declare an instance of a Bittrex wrapper
-wrapper = bittrexWrapper.Wrapper("keys.txt")
 
 # Function to get average fill price based on a Bittrex book
 # Requires a Bittrex API respose for getorderbook
@@ -56,23 +54,37 @@ def get_rate(requestResponse, quantity, marketSide):
         else:
             quantityToFill -= tempQuantity
 
+app = Flask(__name__)
+
+# Thread lock for concurrent requests
+exchangeLock = threading.Lock()
+
+# Declare an instance of a Bittrex wrapper
+wrapper = bittrexWrapper.Wrapper("keys.txt")
+
 # App route for the 'get-fill-price' api call
 @app.route('/api/v1.0/get-fill-price', methods=['GET'])
 def get_fill_price():
     # Check that all three arguments are present, and only three are present
     arguments = ['base-currency', 'counter-currency', 'quantity']
 
+    # Acquire exchange lock
+    exchangeLock.acquire()
+
     # Check that the number of arguments are correct
     if not all(args in arguments for args in request.args) or not len(request.args) == 3:
+        exchangeLock.release()
         return jsonify({'success': False, 'message': 'invalid arguments'})
 
     # Check for valid quantity
     if float(request.args['quantity']) < 0:
+        exchangeLock.release()
         return jsonify({'success': False, 'message': 'invalid quantity'})
 
     # Check that the market is valid
     elif not wrapper.is_valid_market(request.args['base-currency'],
                                      request.args['counter-currency']):
+        exchangeLock.release()
         return jsonify({'success': False, 'message': 'invalid market'})
 
     # Return fill price
@@ -82,6 +94,9 @@ def get_fill_price():
     orderbookResponse = wrapper.get_orderbook(formattedTicker, "buy")
 
     avgPrice = get_average_fill_price(orderbookResponse, quantityToFill)
+
+    # Release lock and return
+    exchangeLock.release()
     return jsonify({'success': True, 'fill-price': avgPrice})
 
 # App route for the 'get-currency-balance' api call
@@ -89,17 +104,25 @@ def get_fill_price():
 def get_currency_balance():
     arguments = ['currency']
 
+    # Acquire exchange lock
+    exchangeLock.acquire()
+
     # Check that the argument currency is present, and that it is the only arg
     if not all(args in arguments for args in request.args) or not len(request.args) == 1:
         # Invalid arguments
+        exchangeLock.release()
         return jsonify({'success': False, 'message': 'invalid arguments'})
 
     # Check for valid currency
     if not wrapper.is_valid_currency(request.args['currency']):
+        exchangeLock.release()
         return jsonify({'success': False, 'message': 'invalid currency'})
 
     # Get currency balance from Bittrex
     balance = wrapper.get_balance(request.args['currency'])['result']['Balance']
+
+    # Release lock and return
+    exchangeLock.release()
     return jsonify({'success': True, 'balance': balance})
 
 # App route for the 'send-order' api call
@@ -107,24 +130,29 @@ def get_currency_balance():
 def send_order():
     arguments = ['base-currency', 'counter-currency', 'order-type', 'amount']
 
+    # Acquire exchange lock
+    exchangeLock.acquire()
+
     # Check that all four arguments are present, and only three are present
     if not all(args in arguments for args in request.form) or not len(request.form) == 4:
+        exchangeLock.release()
         return jsonify({'success': False, 'message': 'invalid arguments'})
 
     # Check for valid amount
     if float(request.form['amount']) < 0:
+        exchangeLock.release()
         return jsonify({'success': False, 'message': 'invalid amount'})
 
     # Check for valid market
     if not wrapper.is_valid_market(request.form['base-currency'],
                                    request.form['counter-currency']):
+        exchangeLock.release()
         return jsonify({'success': False, 'message': 'invalid market'})
 
     # Check for valid order-type
     if not (request.form['order-type'] == 'buy' or request.form['order-type'] == 'sell'):
+        exchangeLock.release()
         return jsonify({'success': False, 'message': 'invalid order-type'})
-
-    print ("TESTING...")
 
     # Get rate to send order at
     quantity = float(request.form['amount'])
@@ -138,30 +166,34 @@ def send_order():
         balance = wrapper.get_balance(request.form['counter-currency'])['result']['Balance']
         # Check that balance of counter-currency * rate is greater than quantity needed
         if balance * orderRate < quantity:
+            exchangeLock.release()
             return jsonify({'success': False, 'message': 'insufficient balance'})
     else:
         balance = wrapper.get_balance(request.form['base-currency'])['result']['Balance']
         # Check that balance is greater than quantity to sell
         if balance < quantity:
+            exchangeLock.release()
             return jsonify({'success': False, 'message': 'insufficient balance'})
 
     # All checks passed, place order
     if request.form['order-type'] == 'buy':
         apiResponse = wrapper.buy_limit(formattedTicker, quantity, orderRate)
         if apiResponse['success'] == True:
+            exchangeLock.release()
             return jsonify({'success': True, 'message': 'order placed'})
         else:
             # Buy request failed
+            exchangeLock.release()
             return jsonify({'success': False, 'message': 'failed to place order'})
     else:
         apiResponse = wrapper.sell_limit(formattedTicker, quantity, orderRate)
         if apiResponse['success'] == True:
+            exchangeLock.release()
             return jsonify({'success': False, 'message': 'failed to place order'})
         else:
             # Sell request failed
+            exchangeLock.release()
             return jsonify({'success': True, 'message': 'order placed'})
-
-
 
 if __name__ == '__main__':
     app.run()
